ed3e493732d26ddcf62cedc047cef20833207c50
nicobarrio <nicolasbarrio97@gmail.com>
2023-05-26 00:33:56 -0300

    some changes

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -9,13 +9,15 @@ const upload = require('../middlewares/productsMulterMiddleware');
 router.get("/products", productsController.list);
 router.get("/products/:id", productsController.detail);
 router.get("/create", productsController.create);
-router.post('/create', 
-    upload.fields([
-        { name: 'image', maxCount: 1 },
-        { name: 'images', maxCount: 6 }]),
+router.post('/create', upload.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'images', maxCount: 6 }]),
     productsController.save);
 router.get("/edit/:id", productsController.edit)
-router.post("/edit/:id", productsController.update);
+router.post("/edit/:id", upload.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'images', maxCount: 6 }]),
+    productsController.update);
 router.delete("/delete/:id", productsController.delete);
 
 module.exports = router;
