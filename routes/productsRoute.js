8280c367790ff315dbdef1b573ed9a468db23ff8
nicobarrio <nicolasbarrio97@gmail.com>
2023-05-30 20:44:01 -0300

    cart view created

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -5,8 +5,9 @@ const productsController = require('../controllers/productsController')
 // MIDDLEWARE //
 const upload = require('../middlewares/productsMulterMiddleware');
 
-
+router.get("/search", productsController.search)
 router.get("/products", productsController.list);
+// router.get("/products/mesas/:id", productsController.listCategory);
 router.get("/products/:id", productsController.detail);
 router.get("/create", productsController.create);
 router.post('/create', upload.fields([
@@ -19,5 +20,6 @@ router.post("/edit/:id", upload.fields([
     { name: 'images', maxCount: 6 }]),
     productsController.update);
 router.delete("/delete/:id", productsController.delete);
+router.get("/cart", productsController.cart);
 
 module.exports = router;
