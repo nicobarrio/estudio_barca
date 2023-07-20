467bd668c44d0a7e0cef4816621727a214fdfb0d
nicobarrio <nicolasbarrio97@gmail.com>
2023-07-20 16:35:42 -0300

    fixed database conection

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "products_db",
-    "host": "127.0.0.1",
+    "username": "uo87ikuhovqrszyd",
+    "password": "cm2c36M0loxLVUgyz9T3",
+    "database": "bevqmq6mcmx0yovhdg52",
+    "host": "bevqmq6mcmx0yovhdg52-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "uo87ikuhovqrszyd",
+    "password": "cm2c36M0loxLVUgyz9T3",
+    "database": "bevqmq6mcmx0yovhdg52",
+    "host": "bevqmq6mcmx0yovhdg52-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "uo87ikuhovqrszyd",
+    "password": "cm2c36M0loxLVUgyz9T3",
+    "database": "bevqmq6mcmx0yovhdg52",
+    "host": "bevqmq6mcmx0yovhdg52-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   }
 }
